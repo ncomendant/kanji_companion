184b8e4664ee722196ea5ff430f35ec0aa8a6810
Nicholas Comendant <nicholas.comendant@gmail.com>
2022-03-28 04:54:48 -0700

    Properly handle overlay click event closures

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # kanji_companion
 Calculates optimal learning order for Japanese Kanji
+
+## Todo
+- [x] Display characters
+- [x] Color code characters by kanji or radical
+- [x] Order characters by order they can be learned 
+- [x] Character overlay
+  - [ ] Stroke order/count
+  - [ ] Example words 
+- [ ] Properly handle overlay click event closures
+- [ ] Pare down term data to improve load times
+- [ ] State pushing and navigation (e.g. proper use of back button)
+- [ ] Custom sort filters
+- [ ] Search bar (for kanji, kana, meanings, and readings)
+  - [ ] Accept romaji input
+- [ ] Export feature
+- [ ] Save search settings for future visits
+- [ ] Improve styling (including improved mobile support)
+- [ ] Enable feedback
--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,12 @@
-use std::{collections::{HashMap, HashSet}, rc::Rc, cell::{RefCell}, ops::Deref, cmp::Ordering};
+use std::{collections::{HashMap, HashSet}, rc::Rc, cell::{RefCell}};
 use enclose::enclose;
 use error::Error;
 use graph::{ReadOnly, Node, Graph};
 use html::Html;
 use wasm_bindgen::{prelude::*, JsCast};
-use wasm_bindgen_futures::JsFuture;
+use wasm_bindgen_futures::{JsFuture};
+use wasm_mutex::Mutex;
 use web_sys::{RequestInit, RequestMode, Request, Response, HtmlElement, MouseEvent, Document};
-use std::hash::Hash;
 
 use regex::Regex;
 
@@ -26,8 +26,6 @@ extern "C" {
 
 pub type Result<T> = std::result::Result<T, error::Error>;
 
-
-
 const WRITING_RE: &str = r"^([^ ]+).+$";
 const WRITING_READING_RE: &str = r"^([^ ]+) \[([^\[\]]+)\].+$";
 
@@ -41,7 +39,23 @@ pub struct Character {
     note: Option<String>,
 }
 
+struct State {
+    overlay_click_closures: Vec<Closure<dyn FnMut(MouseEvent)>>,
+}
 
+impl State {
+    pub fn new() -> Self {
+        State {
+            overlay_click_closures: Default::default(),
+        }
+    }
+}
+
+impl Default for State {
+    fn default() -> Self {
+        State::new()
+    }
+}
 
 async fn fetch_local_text(path: &str) -> Result<String> {
     let location = web_sys::window().expect("no window")
@@ -80,6 +94,7 @@ pub fn document() -> Result<Document> {
 async fn init() -> Result<()> {
     let document = crate::document()?;
     let html = Rc::new(Html::new(&document)?);
+    let state = Rc::new(Mutex::new(State::new()));
 
     set_on_click(&html.overlay.div, |event| {
         event.stop_propagation();
@@ -122,8 +137,10 @@ async fn init() -> Result<()> {
             character_el
         };
 
-        set_on_click(&character_el, enclose!((html, node) move |_event| {
-            on_character_click(html.clone(), &node).unwrap();
+        set_on_click(&character_el, enclose!((html, state, node) move |_event| {
+            wasm_bindgen_futures::spawn_local(enclose!((html, state, node) async move {
+                on_character_click(html.clone(), state.clone(), &node).await.unwrap();
+            }));
         })).forget();
         html.characters.append_child(&character_el)?;
         Ok::<(), error::Error>(())
@@ -133,7 +150,7 @@ async fn init() -> Result<()> {
     Ok(())
 }
 
-fn on_character_click(html: Rc<Html>, node: &ReadOnly<Node<Character>>) -> Result<()> {
+async fn on_character_click(html: Rc<Html>, state: Rc<Mutex<State>>, node: &ReadOnly<Node<Character>>) -> Result<()> {
     let node = node.borrow();
     let character = node.val();
 
@@ -143,45 +160,49 @@ fn on_character_click(html: Rc<Html>, node: &ReadOnly<Node<Character>>) -> Resul
 
     let document = document()?;
 
-    html.overlay.parents.set_inner_html("");
-    node.parents().iter().try_for_each(enclose!((html) |p| {
-        let parent_el = {
-            let p = p.borrow();
-            let parent_el: HtmlElement = document.create_element("div")?.unchecked_into();
-            parent_el.set_text_content(Some(&p.val().writing.to_string()));
-            html.overlay.parents.append_child(&parent_el)?;
-            parent_el
-        };
-        set_on_click(&parent_el, enclose!((html, p) move |_event| {
-            on_character_click(html.clone(), &p).unwrap();
-        })).forget();
-        Ok::<(), Error>(())
-    }))?;
+    {
+        let mut locked_state = state.lock().await;
+        locked_state.overlay_click_closures.clear();
 
-    html.overlay.children.set_inner_html("");
-    node.children().iter().try_for_each(enclose!((html) |c| {
-        let child_el = {
-            let c = c.borrow();
-            let child_el: HtmlElement = document.create_element("div")?.unchecked_into();
-            child_el.set_text_content(Some(&c.val().writing.to_string()));
-            html.overlay.children.append_child(&child_el)?;
-            child_el
-        };
-        set_on_click(&child_el, enclose!((html, c) move |_event| {
-            on_character_click(html.clone(), &c).unwrap();
-        })).forget();
-        Ok::<(), Error>(())
-    }))?;
+        html.overlay.parents.set_inner_html("");
+        node.parents().iter().try_for_each(enclose!((html, state) |p| {
+            let closure = add_overlay_relative(&document, html, state, p, true)?;
+            locked_state.overlay_click_closures.push(closure);
+            Ok::<(), Error>(())
+        }))?;
 
-    // let parents_str = node.parents().iter().map(|p| p.borrow().val().writing.to_string()).collect::<Vec<_>>().join("");
-    // let children_str = node.children().iter().map(|c| c.borrow().val().writing.to_string()).collect::<Vec<_>>().join("");
-
-    // html.overlay.parents.set_text_content(Some(&parents_str));
-    // html.overlay.children.set_text_content(Some(&children_str));
-
+        html.overlay.children.set_inner_html("");
+        node.children().iter().try_for_each(enclose!((html, state) |c| {
+            let closure = add_overlay_relative(&document, html, state, c, false)?;
+            locked_state.overlay_click_closures.push(closure);
+            Ok::<(), Error>(())
+        }))?;
+    }
+    
     html.overlay.wrapper.class_list().remove_1("hidden")?;
 
     Ok(())
+}
+
+fn add_overlay_relative(document: &Document, html: Rc<Html>, state: Rc<Mutex<State>>, relative: &ReadOnly<Node<Character>>, is_parent: bool) -> Result<Closure<dyn FnMut(MouseEvent)>> {
+    let relative_el = {
+        let relative = relative.borrow();
+        let el: HtmlElement = document.create_element("div")?.unchecked_into();
+        el.set_text_content(Some(&relative.val().writing.to_string()));
+        let container = if is_parent {
+            &html.overlay.parents
+        } else {
+            &html.overlay.children
+        };
+        container.append_child(&el)?;
+        el
+    };
+    let closure = set_on_click(&relative_el, enclose!((html, state, relative) move |_event| {
+        wasm_bindgen_futures::spawn_local(enclose!((html, state, relative) async move {
+            on_character_click(html.clone(), state.clone(), &relative).await.unwrap();
+        }));
+    }));
+    Ok(closure)
 }
 
 fn set_on_click<F>(el: &HtmlElement, handler: F) -> Closure<dyn FnMut(MouseEvent)>
